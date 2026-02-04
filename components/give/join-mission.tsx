d38f6c321ce5b0806f274db9a7c91ecc720d2b52
Iyke <ikegwimik@gmail.com>
2026-02-04 08:21:37 +0100

    create give page hero section

---
+++ components/give/join-mission.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+
+export default function JoinMission() {
+  
+  
+
+  return (
+    <section className="w-full py-12 md:py-16 px-4 bg-white">
+      <div className="max-w-7xl mx-auto">
+        {/* Section Header */}
+        <div className="mb-8">
+          <div className="flex items-start justify-between mb-3">
+            <div className="flex-1 text-center">
+              <h2 className="font-display text-2xl md:text-3xl lg:text-4xl font-black text-black leading-tight uppercase">
+                Join the Mission Through Giving
+              </h2>
+            </div>
+            
+          </div>
+          <p className="font-body text-gray-700 text-xs md:text-sm max-w-3xl mx-auto text-center">
+We give because  generosity is the heart of God and the center of our Faith. Every giving helps spread the Gospel, builds lives, and sustains the vision God has entrusted to the Ministry. Your giving is more than a donation, Its an investment in transformed lives and global impact          </p>
+        </div>
+
+       
+      </div>
+    </section>
+  );
+}
