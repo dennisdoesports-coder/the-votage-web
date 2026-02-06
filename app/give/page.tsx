b2fad48afcca81e110ca806bd7adbd28ffe8cc73
Iyke <ikegwimik@gmail.com>
2026-02-06 20:39:59 +0100

    implement bank payment sections

--- app/give/page.tsx
+++ app/give/page.tsx
@@ -2,15 +2,17 @@ import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { GiveHero } from "@/components/give/give-hero";
 import JoinMission from "@/components/give/join-mission";
+import InternationalTransfer from "@/components/give/international-transfer";
 
 export default function GivePage() {
   return (
     <div className="relative w-full overflow-hidden">
       <Navbar />
-      
+
       <main className=" min-h-screen">
         <GiveHero />
         <JoinMission />
+        <InternationalTransfer />
       </main>
 
       <Footer />
